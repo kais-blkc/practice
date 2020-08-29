ae3fd933c691de68a46be8d6ee77ebe62ad1b281
kais <nagliyavarec@gmail.com>
2020-08-29 18:28:50 +0300

    Added modal. (Добавил модалку и исправил скролл при меню и модалке)

--- tokiobuy/js/script.js
+++ tokiobuy/js/script.js
@@ -1,6 +1,31 @@
 addEventListener('DOMContentLoaded', () => {
 	// Модальное окно
-	
+	let modal = document.querySelector('#myModal');
+	let span = document.querySelectorAll('.close')[0];
+
+	document.addEventListener("click", (event) => {
+		if (event.target.closest('.product-body')) {
+			modal.style.opacity = "1";
+			modal.style.visibility = "visible";
+			document.body.style.overflow = "hidden";
+		} else if (event.target.closest('.burger')) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "auto";
+		}
+	});
+
+	span.onclick = () => {
+		modal.style.opacity = "0";
+		modal.style.visibility = "hidden";
+	}
+
+	window.onclick = (event) => {
+		if (event.target == modal) {
+			modal.style.opacity = "0";
+			modal.style.visibility = "hidden";
+		}
+	}
 
 
 
@@ -9,21 +34,31 @@ addEventListener('DOMContentLoaded', () => {
 	let burger = document.querySelector('.burger-click');
 	let link = document.querySelectorAll('.header-link');
 	let black = document.querySelector('.black');
+
 	// Открытие меню
-	burger.addEventListener('click', () => {
+	burger.onclick = () => {
 		close.classList.add('open');
 		black.style.opacity = '1';
 		black.style.visibility = 'visible';
-	});
-	// Открытие меню
-	close.addEventListener('click', () => {
+	};
+
+	// document.onclick = (event) => {
+	// 	if (event.target.closest('.burger')) {
+	// 		document.body.style.overflow = "hidden";
+	// 	} else {
+	// 		document.body.style.overflow = "auto";
+	// 	}
+	// };
+
+	// Закрытие меню
+	close.onclick = () => {
 		close.classList.remove('open');
 		black.style.opacity = '0';
 		black.style.visibility = 'hidden';
-	});
-
+	};
+	
 	document.addEventListener('click', (event) => {
-		console.log(event.target.className);
+		// console.log(event.target.className);
 		if (event.target.className == 'black') {
 			close.classList.remove('open');
 			black.style.opacity = '0';
